5222a36979e7a9e45e254a0a403cd044aae95583
Daniel Klug <daniel.j.klug@gmail.com>
2020-11-07 12:22:14 -0800

    Fixed namespace issue

--- zoom_translate.py
+++ zoom_translate.py
@@ -15,28 +15,11 @@ MOUSE_SCALE      = 1.0
 
 # --------------------------------------------------------------------------- #
 
-def run():
-    if cmds.contextInfo(ZOOM_TRANSLATE, exists=True):
-        cmds.setToolTo(ZOOM_TRANSLATE)
-        return
-
-    cmds.draggerContext( ZOOM_TRANSLATE
-                       , pressCommand   = 'start_translate()'
-                       , dragCommand    = 'do_translate()'
-                       , releaseCommand = 'end_translate()'
-                       , cursor = 'hand'
-                       , undoMode='step')
-    
-    # Put it as the y-key slot
-    mel.eval('$gNonSacredTool = "{}";'.format(ZOOM_TRANSLATE))
-    cmds.setToolTo(ZOOM_TRANSLATE)
-    
-    
-def start_translate():    
+def start_translate():
     global CAMERA
     global MOVE_DATA
     global MOUSE_START
-    
+
     if cmds.draggerContext(ZOOM_TRANSLATE, q=True, bu=True) == 1:
         cmds.setToolTo('selectSuperContext')
         cmds.warning('Use the middle mouse button to slide objects toward/away from camera')
@@ -48,15 +31,15 @@ def start_translate():
         cmds.setToolTo('selectSuperContext')
         print 'Nothing selected'
         return
-    
+
     active_panel = cmds.getPanel(wf=True)
     CAMERA = cmds.modelPanel(active_panel, q=True, cam=True)
     CAMERA = CAMERA.replace('Shape', '')
-    
+
     target_loc = cmds.xform(CAMERA, q=True, ws=True, a=True, rp=True)
     destination_vec = om.MVector(target_loc)
     MOUSE_START = cmds.draggerContext(ZOOM_TRANSLATE, q=True, ap=True)
-    
+
     for each in sel:
         loc = cmds.xform(each, q=True, ws=True, a=True, rp=True)
         vec = om.MVector(loc)
@@ -70,7 +53,7 @@ def do_translate():
 
     manip_scale = MOUSE_SCALE * 0.0025
     manip_amount = mouse_pos * manip_scale
-    
+
     for each in MOVE_DATA.keys():
         vec_scale = MOVE_DATA[each][1] * manip_amount
         vec_manip = MOVE_DATA[each][0] + vec_scale
@@ -82,3 +65,22 @@ def do_translate():
 def end_translate():
     global MOVE_DATA
     MOVE_DATA = {}
+
+
+def run():
+    if cmds.contextInfo(ZOOM_TRANSLATE, exists=True):
+        cmds.setToolTo(ZOOM_TRANSLATE)
+        return
+
+    cmds.draggerContext( ZOOM_TRANSLATE
+
+                       , pressCommand   = 'zoom_translate.start_translate()'
+                       , dragCommand    = 'zoom_translate.do_translate()'
+                       , releaseCommand = 'zoom_translate.end_translate()'
+                       , cursor = 'hand'
+                       , undoMode='step')
+
+    # Put it as the y-key slot
+    mel.eval('$gNonSacredTool = "{}";'.format(ZOOM_TRANSLATE))
+    cmds.setToolTo(ZOOM_TRANSLATE)
+
